af3fcf0ffe8d57deafb765849e8bee1e3d9c8cd0
wangze <zacharywong880@outlook.com>
2021-02-21 13:27:52 +0800

    2021/2/21

---
+++ task2050-2059/2054-A==B?.cpp
@@ -0,0 +1,22 @@
+//
+// Created by 王泽 on 2021/2/21.
+//
+
+#include <iostream>
+#include "iomanip"
+using namespace std;
+/**
+ * 看似简单实际没有那么容易
+ * 原因就是高精度问题，如果是超长的数列呢？？？int、long long、double都放不下呢？
+ * 如果含有浮点类型呢？
+ * 所以这道题要用高精度的思想来做→→→开数组
+ * @return
+ */
+int main(){
+    int a, b;
+    while (cin >> a >> b){
+
+    }
+    return 0;
+}
+
