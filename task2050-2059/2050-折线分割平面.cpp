2328f89ac518ebd41d64495b905510f13339cc57
wangze <zacharywong880@outlook.com>
2021-02-23 17:43:39 +0800

    2021/2/23

--- task2050-2059/2050-折线分割平面.cpp
+++ task2050-2059/2050-折线分割平面.cpp
@@ -1,8 +1,13 @@
+//
+// Created by 王泽 on 2021/2/23.
+//
+
 #include <iostream>
 #include <algorithm>
 #include "iomanip"
 using namespace std;
 /**
+ * 复试考察意义不大
  * 解析：http://blog.sina.com.cn/s/blog_ac5ed4f3010194i8.html
  * @return
  */
