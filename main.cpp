e2e9f8d76cdbeaa2751e466e1c127955ff12c019
wangze <zacharywong880@outlook.com>
2021-02-23 13:21:37 +0800

    2021/2/23

--- main.cpp
+++ main.cpp
@@ -3,19 +3,24 @@
 #include "iomanip"
 using namespace std;
 /**
- * 第一块横放时，放置种数为a[n-2]
- * 第一块竖放时，放置种数为a[n-1]
- * 递推公式为 a[n] = a[n-1] + a[n-2];
+ * 我们令f(n)为长度为n时的种数，即题目要求的输出。
+ * 考虑f(n)时，即考虑n位置能填的字母，n位置能填的字母是受n-1位置字母影响的。
+ * n-1位若为E/F，则n位可以填E/O/F，即f(n) = f(n-1)*3；
+ * n-1位若为O，则n位可以填E/F，即f(n) = f(n-1)*2。
+ * f(n-1)中n-1位为E/F的情况：f(n-2)*2
+ * f(n-1)中n-1位为O的情况自然就是：f(n-1) - f(n-2)*2
+ * 由此可得：
+ * f(n) = ( f(n-2)*2 )*3 + ( f(n-1) - f(n-2)*2 )*2
+ *      = ( f(n-1) + f(n-2) ) * 2
  * @return
  */
 int main(){
     int n;
     while (cin >> n){
-        long long arr[60] = {0,1,2};
-
+        long long arr[50] = {0,3,8};
         if (n>2){
             for (int i = 3; i <= n; ++i) {
-                arr[i] = arr[i-1] + arr[i-2];
+                arr[i] = (arr[i-1] + arr[i-2]) * 2;
             }
         }
         cout << arr[n] << endl;
